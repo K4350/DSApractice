3237f8510dc2a157ea2f960880d77a8b8110cf9c
k4350 <kaushik4350@gmail.com>
2024-05-04 11:47:04 +0530

    BitWise Operator

---
+++ bitwiseOperator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Biwise operator
+
+
+    int a = 5,b = 7;
+
+    cout << " a&b " << (a&b) << endl;
+    cout << " a|b " << (a|b) << endl;
+    cout << " ~a " << (~a) << endl;
+    cout << " a^b " << (a^b) << endl;
+
+
+    return 0;
+}
